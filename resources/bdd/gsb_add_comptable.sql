1aa13459f36cfb21b0d5ce21eb22fb923b0af0c0
JérémyGrmt <59418445+JeremyGrmt@users.noreply.github.com>
2022-10-04 11:04:02 +0200

    Create gsb_add_comptable.sql
    fichier sql qui crée la table comptable et ajoute 3 comptables

---
+++ resources/bdd/gsb_add_comptable.sql
@@ -0,0 +1,23 @@
+drop TABLE if exists comptable;
+
+create table if not exists comptable(
+    id char(4) not null,
+    nom char(30), 
+    prenom char(30),
+    login char(20),
+    mdp char(20),
+    adresse char(30),
+    CP char(5),
+    ville char(30),
+    dateembauche char(30),
+    
+    CONSTRAINT PK_comptable PRIMARY KEY(id)
+);
+
+INSERT INTO comptable(id, nom, prenom, login, mdp, adresse, CP, ville, dateembauche)
+	values(
+        ('a001', 'Lourd', 'Herve', 'hlourd', 'abcdefg38', '1 rue du murrier', '83720', 'Trans-en-provence'), 
+        ('a002', 'Gabin', 'Jean', 'jgabin', 'hijklm83', '2 rue du murrier', '83720', 'Trans-en-provence'),
+        ('a003', 'Matin', 'Martin', 'mmatin', 'nopqrs88', '3 rue du murrier', '83720', 'Trans-en-provence')
+          );
+    
