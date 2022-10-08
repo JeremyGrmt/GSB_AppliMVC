fac34385513c79debaccb259747d21f7cf6ec461
Pascal 'ChaBoxxHF' CROCHARD <pascal.maniak@gmail.com>
2022-10-08 10:46:20 +0200

    changements
    -du nom de la variable visiteur en utilisateur
    - ajout de la variable role

--- src/Controleurs/c_connexion.php
+++ src/Controleurs/c_connexion.php
@@ -29,17 +29,18 @@ switch ($action) {
     case 'valideConnexion':
         $login = filter_input(INPUT_POST, 'login', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $mdp = filter_input(INPUT_POST, 'mdp', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        $visiteur = $pdo->getInfosVisiteur($login, $mdp);
-        if (!is_array($visiteur)) {
+        $utilisateur = $pdo->getInfosVisiteur($login, $mdp);
+        if (!is_array($utilisateur)) {
             Utilitaires::ajouterErreur('Login ou mot de passe incorrect');
             include PATH_VIEWS . 'v_erreurs.php';
             include PATH_VIEWS . 'v_connexion.php';
         } else {
             //la modification de connection se fera ici
-            $id = $visiteur['id'];
-            $nom = $visiteur['nom'];
-            $prenom = $visiteur['prenom'];
-            Utilitaires::connecter($id, $nom, $prenom);
+            $id = $utilisateur['id'];
+            $nom = $utilisateur['nom'];
+            $prenom = $utilisateur['prenom'];
+            $role = $utilisateur['role'];
+            Utilitaires::connecter($id, $nom, $prenom, $role);
             header('Location: index.php');
         }
         break;
