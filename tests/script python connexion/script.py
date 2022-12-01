7fb0a1bd5fe955ec714bb7a4577d183f2932a4d7
Pascal 'ChaBoxxHF' CROCHARD <73274074+ChaBoxxHF@users.noreply.github.com>
2022-12-01 12:48:20 +0100

    ajouts

--- tests/script python connexion/script.py
+++ tests/script python connexion/script.py
@@ -5,7 +5,8 @@ import time
 login = ['lvillachane','dandre','cbedos','ltusseau','pbentot',
 'lbioret','fbunisset','dbunisset','bcacheux','ecadic','ccharoze',
 'cclepkens','vcottin','fdaburon','pde','mdebelle','jdebelle',
-'mdebroise','ndesmarquest','pdesnost','fdudouit','cduncombe']
+'mdebroise','ndesmarquest','pdesnost','fdudouit','cduncombe',
+'cenault','veynde','jfinck','ffremont','agest']
 mdp = ['jux7g','oppg5','gmhxd','ktp3s','doyw1',
 'hrjfs','4vbnd', 's1y1r','uf7r3','6u8dc',
 'u817o','bw1us','2hoh9','7oqpv','gk9kx',
@@ -13,7 +14,7 @@ mdp = ['jux7g','oppg5','gmhxd','ktp3s','doyw1',
 '44im8','qf77j','y2qdu','i7sn3','mpb3t',
 'xs5tq','dywvt']
 code = '1234'
-domainName= 'gsb2'
+domainName= 'gsb-crochard'
 
 driver = webdriver.Chrome()
 driver.get("http://"+domainName+"/index.php?uc=connexion&action=valideConnexion")
