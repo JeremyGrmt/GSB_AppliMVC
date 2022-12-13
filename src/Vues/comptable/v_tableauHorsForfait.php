07defa5f4cc51c83ae46186ac5c06cae41f7b379
jeremyGrmt <59418445+JeremyGrmt@users.noreply.github.com>
2022-12-13 01:55:09 +0100

    edition lignes frais hors forfait ok
    probablement optimisable mais la il est 2h du mat'

--- src/Vues/comptable/v_tableauHorsForfait.php
+++ src/Vues/comptable/v_tableauHorsForfait.php
@@ -15,7 +15,7 @@
  */
 ?>
 <hr>
-<form method="post" role="form">
+<!--<form method="post" role="form">-->
  <!--<form action="index.php?uc=validFicheFrais&action=validerMajFraisHorsForfait" method="post" role="form">-->
 <div class="row">
     <div class="panel panel-info">
@@ -32,27 +32,24 @@
             </thead>  
             <tbody>
 <?php
+$i =0;
 foreach ($lesFraisHorsForfait as $unFraisHorsForfait) {
     $libelle = htmlspecialchars($unFraisHorsForfait['libelle']);
     $date = $unFraisHorsForfait['date'];
     $montant = $unFraisHorsForfait['montant'];
     $id = $unFraisHorsForfait['id'];
     ?>           
-                    <tr>
-                        
-                        <td><input class="input-table" type="text" id="date" name="date"  required value= <?php echo  '"' .  $date . '"'  ?>></td>
-                        <td><input class="input-table" type="text" id="libelle" name="libelle" required value= <?php echo '"' . $libelle . '"' ?>></td>
-                        <td><input class="input-table" type="text" id="montant" name="montant" required value= <?php echo '"' . $montant . '"' ?>></td>
+                    <tr>       
+                        <td><form id="form<?php echo $i ?>" action="index.php?uc=validFicheFrais&action=validerMajFraisHorsForfait&id=<?php echo  $id ?>" method="POST"><input class="input-table" type="text" id="date" name="date"  required value= <?php echo  '"' .  $date . '"'  ?>></form></td>
+                        <td><input class="input-table" form="form<?php echo $i ?>" type="text" id="libelle" name="libelle" required value= <?php echo '"' . $libelle . '"' ?>></td>
+                        <td><input class="input-table" form="form<?php echo $i ?>" type="text" id="montant" name="montant" required value= <?php echo '"' . $montant . '"' ?>></td>
                         <td>
-                            <button class="btn btn-success" type="submit" formaction="index.php?uc=validFicheFrais&action=validerMajFraisHorsForfait&id=<?php echo  $id ?> ">Corriger</button>
+                            <button class="btn btn-success" type="submit" form="form<?php echo $i ?>">Corriger</button>
                             <button class="btn btn-danger" type="reset">Réinitialiser</button>
                         </td> 
-                    
-
-                
-
                 </tr>
                 <?php
+                $i++;
             }
             ?>
             </tbody>  
@@ -60,7 +57,7 @@ foreach ($lesFraisHorsForfait as $unFraisHorsForfait) {
 
     </div>
 </div>
-          </form>
+          <!--</form>-->
 <div class="row">
     <form action="action"></form>
 </div>
